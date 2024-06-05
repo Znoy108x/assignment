dff76273c822e0e971619038f92817a8b21318cc
Abhay Bisht <abhaybisht@Abhays-MacBook-Air.local>
2024-06-05 13:42:39 +0530

    prisma

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,17 +1,23 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { handleLogout } from "@/shared/actions/logout.action";
+import { useState } from "react";
+import ButtonLoading from "../_components/ButtonLoading";
 
 export default function Home() {
 
+  const [isLogout, setIsLogout] = useState<boolean>(false)
+
   const handleLogoutButtonClick = () => {
+    setIsLogout(true)
     handleLogout()
+    setIsLogout(false)
   }
 
   return (
     <div className="w-full h-full flex items-center justify-center bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-      <Button onClick={handleLogoutButtonClick}>
-        Log Out
+      <Button onClick={handleLogoutButtonClick} disabled={isLogout}>
+        <ButtonLoading text="Log Out" isLoading={isLogout} />
       </Button>
     </div>
   );
